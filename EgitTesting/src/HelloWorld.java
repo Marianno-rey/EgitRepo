4db176d03b67a4109ea828c3742ee4643b732247
Marianno <mariannoreynoso@gmail.com>
2021-03-22 13:24:25 -0500

    This is my first commit

---
+++ EgitTesting/src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		int bruh = sc.nextInt();
+		System.out.println(bruh);
+	}
+
+}
