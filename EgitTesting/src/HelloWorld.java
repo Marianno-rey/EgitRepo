b4e89958ebb55c93a5bb2c1292cb86d8d0e802d2
Marianno <mariannoreynoso@gmail.com>
2021-03-22 13:45:53 -0500

    New feature added

--- EgitTesting/src/HelloWorld.java
+++ EgitTesting/src/HelloWorld.java
@@ -1,12 +1,10 @@
-import java.util.*;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		Scanner sc = new Scanner(System.in);
-		int bruh = sc.nextInt();
-		System.out.println(bruh);
 		System.out.println("New line added");
+		System.out.println("This is new feature addition");
 	}
 
 }
